014cb313f1e520e9d92320ab2237bcfa39db33c3
infinityfin <140198482+infinityfin@users.noreply.github.com>
2024-01-09 17:57:25 +0100

    fix

--- start-app.bat
+++ start-app.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Starting Node.js server...
-cd /d ".\app"
+cd /d ".\dev"
 start node server.js
 pause
--- update-app.bat
+++ update-app.bat
@@ -1,5 +1,14 @@
 @echo off
-echo Starting Node.js server...
-cd /d ".\KL"
-start node server.js
+
+set repositoryUrl=https://github.com/YeahsTech-Innovatiions/kl-app.git
+
+echo Cloning or pulling changes from %repositoryUrl%...
+
+if not exist ".git" (
+    git clone %repositoryUrl% .
+) else (
+    git pull origin main  REM Change 'main' to your branch if it's different
+)
+
+echo Done.
 pause
