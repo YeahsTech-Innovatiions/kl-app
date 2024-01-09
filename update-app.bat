b3d16d746f18ea45d9e288d964ecdc2fe5579310
infinityfin <140198482+infinityfin@users.noreply.github.com>
2024-01-09 18:04:29 +0100

    scripts

--- update-app.bat
+++ update-app.bat
@@ -4,11 +4,9 @@ set repositoryUrl=https://github.com/YeahsTech-Innovatiions/kl-app.git
 
 echo Cloning or pulling changes from %repositoryUrl%...
 
-if not exist ".git" (
-    git clone %repositoryUrl% .
-) else (
-    git pull origin main  REM Change 'main' to your branch if it's different
-)
+git pull
+cd /d ".\dev"
+start npm install
 
 echo Done.
 pause
